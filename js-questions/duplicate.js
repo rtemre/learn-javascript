39bad5806fd84b9a7c7eb7e788a819c35fd9887b
Raman Temre <ramantemre2110@gmail.com>
2025-08-30 12:11:22 +0530

    Contain duplicate

---
+++ js-questions/duplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  return new Set(nums).size !== nums.length;
+};
+
+containsDuplicate([1, 2, 3, 1]);
