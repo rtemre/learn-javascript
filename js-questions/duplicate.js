99ab8be8cc30b2cb4dcd4c7a347a409c5f877f76
Raman Temre <ramantemre2110@gmail.com>
2025-08-31 15:13:26 +0530

    Contains Near by Duplicate

--- js-questions/duplicate.js
+++ js-questions/duplicate.js
@@ -7,3 +7,36 @@ var containsDuplicate = function (nums) {
 };
 
 containsDuplicate([1, 2, 3, 1]);
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (Math.abs(i - map.get(nums[i])) <= k) {
+      return true;
+    }
+    map.set(nums[i], i);
+  }
+
+  return false;
+};
+
+containsNearbyDuplicate([1, 2, 3, 1], 3);
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
