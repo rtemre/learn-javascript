682cb2bf0ea10ac51184223d924cb28bb17f3b27
Raman Temre <ramantemre2110@gmail.com>
2025-09-21 11:16:42 +0530

    Isomorphic string

---
+++ js-questions/isomorphic_string.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i], i + 1) !== t.indexOf(t[i], i + 1)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let s = "egg",
+  t = "add";
+console.log(isIsomorphic(s, t));
+
+// Example 1:
+// Input: s = "egg", t = "add"
+// Output: true
+// Explanation:
+// The strings s and t can be made identical by:
+// Mapping 'e' to 'a'.
+// Mapping 'g' to 'd'.
+
+// Example 2:
+// Input: s = "foo", t = "bar"
+// Output: false
+// Explanation:
+// The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+
+// Example 3:
+// Input: s = "paper", t = "title"
+// Output: true
