a94d1bcd7c1586052ec79a815253522b47743d58
Raman Temre <ramantemre2110@gmail.com>
2025-10-01 17:50:19 +0530

    longest substring

--- js-questions/substring.js
+++ js-questions/substring.js
@@ -15,3 +15,17 @@ printAllSubstrings("abc");
 // b
 // bc
 // c
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var longestSubstring = function (s, k) {
+  for (const char of Array.from(new Set(s))) {
+    if (s.match(new RegExp(char, "g")).length < k) {
+      return Math.max(...s.split(char).map((str) => longestSubstring(str, k)));
+    }
+  }
+  return s.length;
+};
