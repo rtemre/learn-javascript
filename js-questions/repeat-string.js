56bddb448556df129e77f279e166690450752f2a
Raman Temre <ramantemre2110@gmail.com>
2025-10-11 23:59:06 +0530

    Repeat string match

---
+++ js-questions/repeat-string.js
@@ -0,0 +1,8 @@
+var repeatedStringMatch = function (A, B) {
+  const length = Math.ceil(B.length / A.length);
+  if (A.repeat(length).includes(B)) return length;
+  if ((A.repeat(length) + A).includes(B)) return length + 1;
+  return -1;
+};
+
+repeatedStringMatch("abcd", "cdabcdab");
