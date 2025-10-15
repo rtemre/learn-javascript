aa9f12727e520f5256944f2ef4b17514e4858d3e
Raman Temre <ramantemre2110@gmail.com>
2025-10-15 23:56:07 +0530

    Convert to base 7

---
+++ js-questions/base7.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+  return num.toString(7);
+};
+
+console.log(convertToBase7(100)); // "202"
+console.log(convertToBase7(-7)); // "-10"
+console.log(convertToBase7(0)); // "0"
